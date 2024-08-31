75ed079435a1016ef3ee9375fc86712c6b65b84e
1orzero <laokameng.6@gmail.com>
2024-09-01 00:57:51 +0800

    feat(cli): add initial implementation of git-helper-cli

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/1orzero/git-helper-cli/internal/branch"
+	"github.com/1orzero/git-helper-cli/internal/config"
+	"github.com/1orzero/git-helper-cli/internal/openai"
+	"github.com/1orzero/git-helper-cli/internal/utils"
+)
+
+func main() {
+	config := config.LoadConfig()
+	description := utils.GetDescription()
+	llm := openai.InitializeOpenAIClient(config.APIEndpoint, config.APISecret)
+	branchNames := branch.GenerateAndCleanBranchNames(llm, description, config.Username)
+	selectedBranch := branch.SelectBranchName(branchNames)
+	utils.CopyToClipboard(selectedBranch)
+}
