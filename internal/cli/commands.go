9392f600ce915d5cbc6f44f136e3323897f9c66d
1orzero <laokameng.6@gmail.com>
2024-10-07 22:48:52 +0800

    feat(cli): modify command to accept app state parameter

--- internal/cli/commands.go
+++ internal/cli/commands.go
@@ -4,42 +4,36 @@ import (
 	"strings"
 
 	"github.com/1orzero/git-helper-cli/internal/branch"
-	"github.com/1orzero/git-helper-cli/internal/config"
-	"github.com/1orzero/git-helper-cli/internal/openai"
+	"github.com/1orzero/git-helper-cli/internal/state"
 	"github.com/1orzero/git-helper-cli/internal/utils"
 	ucli "github.com/urfave/cli/v2"
 )
 
-func Commands() []*ucli.Command {
+func Commands(appState *state.AppState) []*ucli.Command {
 	return []*ucli.Command{
 		{
 			Name:      "generate-branch-name",
 			Usage:     "Generate a branch name based on the issue type and title",
-			Action:    generateBranchName,
+			Action:    generateBranchName(appState),
 			ArgsUsage: "<description>",
 		},
 	}
 }
 
-func generateBranchName(c *ucli.Context) error {
-	cfg, err := config.LoadConfig(c.String("config"))
-	if err != nil {
-		return ucli.Exit("Error loading config: "+err.Error(), 1)
+func generateBranchName(appState *state.AppState) func(c *ucli.Context) error {
+	return func(c *ucli.Context) error {
+		description := strings.Join(c.Args().Slice(), " ")
+		if description == "" {
+			return ucli.Exit("Description is required", 1)
+		}
+
+		branchNames := branch.GenerateAndCleanBranchNames(*appState.LLM, description, *appState.Config)
+
+		// Select a branch name from the list (using fzf)
+		selectedBranch := branch.SelectBranchName(branchNames)
+		utils.CopyToClipboard(selectedBranch)
+
+		Output(selectedBranch)
+		return nil
 	}
-	description := strings.Join(c.Args().Slice(), " ")
-	if description == "" {
-		return ucli.Exit("Description is required", 1)
-	}
-
-	// Initialize OpenAI client
-	llm := openai.InitializeOpenAIClient(cfg.API.APIEndpoint, cfg.API.APISecret)
-
-	branchNames := branch.GenerateAndCleanBranchNames(llm, description, cfg)
-
-	// Select a branch name from the list (using fzf)
-	selectedBranch := branch.SelectBranchName(branchNames)
-	utils.CopyToClipboard(selectedBranch)
-
-	Output(selectedBranch)
-	return nil
 }
